daae0559cdf217c93ac97825bb099c25a59149d2
igorgs <igor.gs@hotmail.com>
2021-02-14 01:46:59 -0300

    Mathematical Surfaces Complete

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -3,50 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Static;
 
-namespace Graph
+public class Graph : MonoBehaviour
 {
-    public class Graph : MonoBehaviour
+
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField, Range(10,100)]
+    int resolution = 10;
+
+    [SerializeField]
+	FunctionLibrary.FunctionName function = default;
+
+    Transform[] points;
+
+    void Awake()
     {
-
-        [SerializeField]
-        Transform pointPrefab = default;
-
-        [SerializeField, Range(10,100)]
-        int resolution = 10;
-
-        void Awake()
-        {
-
-            float step = 2f / resolution;
-            Vector3 position = Vector3.zero;
-            Vector3 scale = Vector3.one * step;
-
-            for (int i = 0; i < resolution; i++) {
-
-                Transform point = Instantiate(pointPrefab);
-
-                position.x = (i + 0.5f) * step - 1f;
-                position.y = Formulas.Cubic(position.x);
-                point.localPosition = position;
-                point.localScale = scale;
-
-                point.SetParent(transform, false);
-
-            }
-        }
+        float step = 2f / resolution;
+		var scale = Vector3.one * step;
+		//var position = Vector3.zero;
+		points = new Transform[resolution * resolution];
+		for (int i = 0; i < points.Length; i++) {
+			//if (x == resolution) {
+			//	x = 0;
+			//	z += 1;
+			//}
+			Transform point = Instantiate(pointPrefab);
+			//position.x = (x + 0.5f) * step - 1f;
+			//position.z = (z + 0.5f) * step - 1f;
+			//point.localPosition = position;
+			point.localScale = scale;
+			point.SetParent(transform, false);
+			points[i] = point;
+		}
+    }
 
         // Start is called before the first frame update
-        void Start()
-        {
+    void Start()
+    {
             
-        }
+    }
 
         // Update is called once per frame
-        void Update()
-        {
-            
-        }
-
+    void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+		float time = Time.time;
+		float step = 2f / resolution;
+		float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution) {
+				x = 0;
+				z += 1;
+				v = (z + 0.5f) * step - 1f;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
+		}
     }
-}
 
+}
